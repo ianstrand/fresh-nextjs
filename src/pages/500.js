ce68c486064f3a69030ea95d16a7d2f1fac8518c
Ian Strand <strandoutmedia@gmail.com>
2021-12-17 00:31:30 -0800

    error

---
+++ src/pages/500.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
+export default function Custom500() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace("/500/")
+  })
+
+  return null
+}
