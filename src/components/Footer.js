37646ea1b692b0a209fb5fed88f3df5be257686b
Ian Strand <strandoutmedia@gmail.com>
2021-12-18 14:48:26 -0800

    Google analytics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,6 +23,7 @@ export default class Footer extends React.Component {
                         ))}
                     </p>
                 )}
+                
             </footer>
         );
     }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,43 @@
 import '../sass/main.scss';
+import { useEffect } from "react";
+import Document, { NextScript } from 'next/document';
+import { useRouter } from "next/router";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+    return (
+        <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXX`}
+      />
+      <script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <Component {...pageProps} />
+        </>
+      );
 }
